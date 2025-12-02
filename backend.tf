ac1eee5a589a95aba073039e4d66128c7466d210
skbez1996 <40964137+skbez1996@users.noreply.github.com>
2025-12-01 22:23:06 -0600

    Update S3 key path for Terraform backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "engdata2025"
-    key          = "mytfstate.devops/terraform.tfstate"
+    key          = "mytfstate/devops/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
