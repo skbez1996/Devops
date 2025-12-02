a5cf5974f9af51b27718cc3181eadea65aafa69f
skbez1996 <40964137+skbez1996@users.noreply.github.com>
2025-12-01 21:10:40 -0600

    Add files via upload
    added files to create a s3 bucket

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# S3 Bucket
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "test-sb-demo-bucket-12345"   # <-- change this to a unique name
+}
+
+# Enable versioning (optional)
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
