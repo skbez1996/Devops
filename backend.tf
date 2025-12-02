255b05d60bef1172c273998af383c6ae5af40bf0
skbez1996 <40964137+skbez1996@users.noreply.github.com>
2025-12-01 22:10:28 -0600

    Fix formatting in backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "engdata2025"
-    key            = "mytfstate.devops/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    use_lockfile   = true
+    bucket       = "engdata2025"
+    key          = "mytfstate.devops/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
