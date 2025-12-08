b2ccc3c83220c0967b3423925cb3a3e47344d97c
skbez1996 <40964137+skbez1996@users.noreply.github.com>
2025-12-08 03:47:59 +0000

    added vpcs

--- main.tf
+++ main.tf
@@ -13,6 +13,90 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# VPC for EKS
+resource "aws_vpc" "eks_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "eks-vpc"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = {
+    Name = "eks-igw"
+  }
+}
+
+# Public Subnets in 3 Availability Zones
+resource "aws_subnet" "az1" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "eks-subnet-az1"
+  }
+}
+
+resource "aws_subnet" "az2" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "eks-subnet-az2"
+  }
+}
+
+resource "aws_subnet" "az3" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "eks-subnet-az3"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "eks_rt" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks_igw.id
+  }
+
+  tags = {
+    Name = "eks-route-table"
+  }
+}
+
+# Route Table Associations
+resource "aws_route_table_association" "az1" {
+  subnet_id      = aws_subnet.az1.id
+  route_table_id = aws_route_table.eks_rt.id
+}
+
+resource "aws_route_table_association" "az2" {
+  subnet_id      = aws_subnet.az2.id
+  route_table_id = aws_route_table.eks_rt.id
+}
+
+resource "aws_route_table_association" "az3" {
+  subnet_id      = aws_subnet.az3.id
+  route_table_id = aws_route_table.eks_rt.id
+}
+
 # S3 Bucket
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "test-sb-demo-bucket-67678989" # <-- change this to a unique name
